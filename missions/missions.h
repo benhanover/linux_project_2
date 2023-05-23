d49f9313b18f7480f6d0cd8e214d20093b4f9f94
lahav <lahav.surf@gmail.com>
2023-05-23 13:00:06 +0300

    finish missions 2

--- missions/missions.h
+++ missions/missions.h
@@ -1,22 +1,21 @@
 #pragma once
-
 #include "../utilities/utility.h"
 #include <sstream>
 
 
-// functions
+//--------------------------functions------------------------------
 void getInputFromUser(vector<string>& words, string message);
 void notAllInDB(vector<string>& missing_names, System& airports);
 
-//mission 1
+//--------------------------mission 1------------------------------
 
 void printSingleAirportArv(System& airports,string& IcoaCode);
 void printAirportsArv(System& airports, vector<string> paths);
-//mission 2
+//--------------------------mission 2------------------------------
 
 void printAirportSchedule(System& airports, vector<string> paths);
 
-//mission 3
+//--------------------------mission 3------------------------------
 
-//mission 4
+//--------------------------mission 4------------------------------
 void refreshDataBase(System& airports, vector<string>paths);
